c2336f3d488d791058233e80646ab184b100ea10
DylanToporek <dylantoporek@gmail.com>
2021-10-10 11:36:16 -0400

    commit tree ebd28151a5511030519b17d9ff31e35773205894

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick')
+//const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(res => res.json())
+.then(data => imgHandler(data))
+
+function imgHandler(obj){
+    imgArr = [...obj.message]
+    let imgBox = document.querySelector("#dog-image-container")
+    imgArr.forEach(element => {
+        let newDog = document.createElement('img')
+        newDog.src = element
+        imgBox.appendChild(newDog)
+
+    })
+}
+
+fetch('https://dog.ceo/api/breeds/list/all')
+.then(res => res.json())
+.then(data => breedHandler(data))
+
+function breedHandler(obj){
+    let breeds = obj.message
+    let list = document.querySelector("#dog-breeds")
+    for (const breed in breeds){
+        let newBreed = document.createElement('li')
+        newBreed.setAttribute('class', 'dog')
+        newBreed.innerHTML = breed
+        list.appendChild(newBreed)
+    }
+}
+
